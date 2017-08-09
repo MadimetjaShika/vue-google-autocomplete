8bd879353f0bb2b6c4a19fff3882d522838561d1
Madimetja Shika <mmjshika@gmail.com>
2017-08-09 15:29:39 +0200

    updated the demo output code

--- example/src/app.js
+++ example/src/app.js
@@ -5,68 +5,92 @@ import VuetifyGoogleAutocomplete from './../../src/VuetifyGoogleAutocomplete.vue
 Vue.use(Vuetify);
 
 const app = new Vue({
-    el: '#app',
+  el: '#app',
 
-    components: { VuetifyGoogleAutocomplete },
+  components: { VuetifyGoogleAutocomplete },
 
-    data: {
-        vueGoogleAutocompleteLink: 'https://github.com/olefirenko/vue-google-autocomplete',
-        gitHubLink: 'https://github.com/MadimetjaShika/vuetify-google-autocomplete',
-        npmLink: 'https://www.npmjs.com/package/vuetify-google-autocomplete',
-        address: {},
-        disabled: false,
-        id: 'map',
-        appendIcon: 'search',
-        prependIcon: '',
-        callbackFunction: 'getAddressData',
-        classname: '',
-        placeholderText: 'Search Address',
-        types: 'address',
-        typesOptions: [
-            'geocode',
-            'address',
-            'establishment',
-            'regions',
-            'cities',
-        ],
-        country: [],
-        countryOptions: [
-            'br',
-            'sg',
-            'fr',
-            'za',
-            'mx'
-        ],
-        enableGeolocation: false,
+  data: {
+    vueGoogleAutocompleteLink: 'https://github.com/olefirenko/vue-google-autocomplete',
+    gitHubLink: 'https://github.com/MadimetjaShika/vuetify-google-autocomplete',
+    npmLink: 'https://www.npmjs.com/package/vuetify-google-autocomplete',
+    address: {},
+    disabled: false,
+    id: 'map',
+    appendIcon: 'search',
+    prependIcon: '',
+    callbackFunction: 'getAddressData',
+    classname: '',
+    placeholderText: 'Search Address',
+    types: 'address',
+    typesOptions: [
+      'geocode',
+      'address',
+      'establishment',
+      'regions',
+      'cities',
+    ],
+    country: [],
+    countryOptions: [
+      'br',
+      'sg',
+      'fr',
+      'za',
+      'mx'
+    ],
+    enableGeolocation: false,
+  },
+
+  computed: {
+    outputHtml: function(){
+      return `<vuetify-google-autocomplete
+    id="${this.id}"
+    :append-icon="${this.appendIcon}"
+    :prepend-icon="${this.prependIcon}"
+    :classname="${this.classname}"
+    :placeholder="${this.placeholderText}"
+    :disabled="${this.disabled}"
+    :enable-geolocation="${this.enableGeolocation}"
+    :types="${this.types}"
+    :country="${this.country}"
+    :placechanged="${this.callbackFunction}">
+ </vuetify-google-autocomplete>`;
     },
 
-    computed: {
-        outputHtml: function(){
-            console.log('Hello output...');
-            return `<vuetify-google-autocomplete
-        id="${this.id}"
-        :appendIcon="${this.appendIcon}"
-        :prependIcon="${this.prependIcon}"
-        :classname="${this.classname}"
-        :placeholder="${this.placeholderText}"
-        :disabled="${this.disabled}"
-        :enable-geolocation="${this.enableGeolocation}"
-        :types="${this.types}"
-        :country="${this.country}"
-        :placechanged="${this.callbackFunction}">
-</vuetify-google-autocomplete>`;
-        },
-    },
-
-    methods: {
-        /**
-        * Callback method when the location is found.
-        *
-        * @param {Object} addressData Data of the found location
-        * @param {Object} placeResultData PlaceResult object
-        */
-        getAddressData: function (addressData, placeResultData) {
-            this.address = addressData;
-        }
+    outputJsData: function(){
+      return `data() {
+    return {
+      address: ${JSON.stringify(this.address)},
+      disabled: ${this.disabled},
+      id: '${this.id}',
+      appendIcon: '${this.appendIcon}',
+      prependIcon: '${this.prependIcon}',
+      classname: '${this.classname}',
+      placeholderText: '${this.placeholderText}',
+      types: '${this.types}',
+      country: [${this.country}],
+      enableGeolocation: ${this.enableGeolocation},
     }
+ }`
+    },
+
+    outputJsCallback: function(){
+      return `methods: {
+    ${this.callbackFunction}: function (addressData, placeResultData) {
+      this.address = addressData;
+    }
+ }`;
+    },
+  },
+
+  methods: {
+    /**
+    * Callback method when the location is found.
+    *
+    * @param {Object} addressData Data of the found location
+    * @param {Object} placeResultData PlaceResult object
+    */
+    getAddressData: function (addressData, placeResultData) {
+      this.address = addressData;
+    }
+  }
 });
