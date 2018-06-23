e3e95d791794dc371233bdd8d00cc18b8607d51e
Madimetja Shika <mmjshika@gmail.com>
2018-06-24 01:15:49 +0200

    WIP on docs

--- update-docs.js
+++ update-docs.js
@@ -1,7 +1,7 @@
 const jsdoc2md = require('jsdoc-to-markdown');
 const fsPath = require('fs-path');
 
-jsdoc2md.render({ files: './src/*.js' }).then((result) => {
+jsdoc2md.render({ files: './src/vga/VuetifyGoogleAutocomplete.js' }).then((result) => {
   let header = '# Vuetify Google Autocomplete\n\n[![Build Status](https://travis-ci.org/MadimetjaShika/vuetify-google-autocomplete.svg?branch=dev)](https://travis-ci.org/MadimetjaShika/vuetify-google-autocomplete)\n\nFor more detailed HTML output of the JSDocs, refer to the \`\`./docs\`\` directory.\n\n';
 
   const finalResult = `${header}${result}`;
